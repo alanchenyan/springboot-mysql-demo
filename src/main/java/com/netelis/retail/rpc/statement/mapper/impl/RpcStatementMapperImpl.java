afbe386195e90aa5946de655dfe00a0dee44aa6e
alanchen <chenyan900520@126.com>
2019-05-17 09:57:35 +0800

    增加数据库异常

--- src/main/java/com/netelis/retail/rpc/statement/mapper/impl/RpcStatementMapperImpl.java
+++ src/main/java/com/netelis/retail/rpc/statement/mapper/impl/RpcStatementMapperImpl.java
@@ -1,9 +1,12 @@
 package com.netelis.retail.rpc.statement.mapper.impl;
 
+import com.netelis.retail.common.exception.DatabaseException;
 import com.netelis.retail.config.sqlsession.SqlSessionConfig;
 import com.netelis.retail.rpc.statement.mapper.IRpcStatementMapper;
 import org.apache.ibatis.session.SqlSession;
 import org.mybatis.spring.SqlSessionFactoryBean;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -18,13 +21,15 @@ import java.util.List;
 @Component
 public class RpcStatementMapperImpl implements IRpcStatementMapper {
 
+    private Logger logger = LoggerFactory.getLogger(RpcStatementMapperImpl.class);
+
     @Autowired
     SqlSessionConfig sqlSessionConfig;
 
     @Override
     public String insert(String statement, Object entity) {
         String keyId = null;
-        int affectedRowsNum = 0;
+        int affectedRowsNum;
         SqlSession session = null;
         SqlSessionFactoryBean factory = sqlSessionConfig.createSqlSessionFactory();
         try {
@@ -32,9 +37,12 @@ public class RpcStatementMapperImpl implements IRpcStatementMapper {
             affectedRowsNum = session.insert(statement, entity);
             session.commit();
         } catch (Exception e) {
-            e.printStackTrace();
+            logger.error("SQL异常", e);
+            throw new DatabaseException("SQL异常:" + e.getMessage());
         } finally {
-            session.close();
+            if (session != null) {
+                session.close();
+            }
         }
 
         if (affectedRowsNum == 1) {
@@ -58,9 +66,12 @@ public class RpcStatementMapperImpl implements IRpcStatementMapper {
             session.update(statement, entity);
             session.commit();
         } catch (Exception e) {
-            e.printStackTrace();
+            logger.error("SQL异常", e);
+            throw new DatabaseException("SQL异常:" + e.getMessage());
         } finally {
-            session.close();
+            if (session != null) {
+                session.close();
+            }
         }
     }
 
@@ -73,16 +84,19 @@ public class RpcStatementMapperImpl implements IRpcStatementMapper {
             session.delete(statement, entity);
             session.commit();
         } catch (Exception e) {
-            e.printStackTrace();
+            logger.error("SQL异常", e);
+            throw new DatabaseException("SQL异常:" + e.getMessage());
         } finally {
-            session.close();
+            if (session != null) {
+                session.close();
+            }
         }
     }
 
 
     @Override
     public List selectList(String statement, Object parameter) {
-        List list = null;
+        List list;
         SqlSession session = null;
         SqlSessionFactoryBean factory = sqlSessionConfig.createSqlSessionFactory();
         try {
@@ -90,16 +104,19 @@ public class RpcStatementMapperImpl implements IRpcStatementMapper {
             list = session.selectList(statement, parameter);
             session.commit();
         } catch (Exception e) {
-            e.printStackTrace();
+            logger.error("SQL异常", e);
+            throw new DatabaseException("SQL异常:" + e.getMessage());
         } finally {
-            session.close();
+            if (session != null) {
+                session.close();
+            }
         }
         return list;
     }
 
     @Override
     public Object selectOne(String statement, Object parameter) {
-        Object result = null;
+        Object result;
         SqlSession session = null;
         SqlSessionFactoryBean factory = sqlSessionConfig.createSqlSessionFactory();
         try {
@@ -107,9 +124,12 @@ public class RpcStatementMapperImpl implements IRpcStatementMapper {
             result = session.selectOne(statement, parameter);
             session.commit();
         } catch (Exception e) {
-            e.printStackTrace();
+            logger.error("SQL异常", e);
+            throw new DatabaseException("SQL异常:" + e.getMessage());
         } finally {
-            session.close();
+            if (session != null) {
+                session.close();
+            }
         }
         return result;
     }
